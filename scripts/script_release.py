079a4fee507e36f93c1839ba9b81c1ee7a7a51c9
WhiteWalker608 <nitingoel608@gmail.com>
2021-05-07 10:29:39 +0530

    change https to http (#50)

--- scripts/script_release.py
+++ scripts/script_release.py
@@ -36,43 +36,43 @@ with open(beta_file_path, "r") as beta_file:
 
         public_version = str(v.major) + '.' + str(v.minor) + '.' + str(v.patch)
         if project_name == "beacon":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["beacon"] = "https://public.artifacts.marlin.pro/projects/beacon/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["beacon"] = "http://public.artifacts.marlin.pro/projects/beacon/" + public_version \
                 + "/beacon-linux_amd64"
         
         elif project_name == "relay_eth":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["relay"] = "https://public.artifacts.marlin.pro/projects/relay_eth/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["relay"] = "http://public.artifacts.marlin.pro/projects/relay_eth/" + public_version \
                 + "/relay_eth-linux_amd64"
-            d["bundles"]["linux-amd64.supervisor"]["data"]["geth"] = "https://public.artifacts.marlin.pro/projects/relay_eth/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["geth"] = "http://public.artifacts.marlin.pro/projects/relay_eth/" + public_version \
                 + "/geth-linux_amd64"
             
         elif project_name == "relay_iris" or project_name == "relay_cosmos":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["relay"] = "https://public.artifacts.marlin.pro/projects/" + project_name + "/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["relay"] = "http://public.artifacts.marlin.pro/projects/" + project_name + "/" + public_version \
                 + "/" + project_name + "-linux_amd64"
             
         elif project_name == "gateway_eth":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "https://public.artifacts.marlin.pro/projects/gateway_eth/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "http://public.artifacts.marlin.pro/projects/gateway_eth/" + public_version \
                 + "/gateway_eth-linux_amd64"
         
         elif project_name == "gateway_near":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "https://public.artifacts.marlin.pro/projects/gateway_near/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "http://public.artifacts.marlin.pro/projects/gateway_near/" + public_version \
                 + "/gateway_near-linux_amd64"
 
         elif project_name == "gateway_iris":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "https://public.artifacts.marlin.pro/projects/gateway_iris/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "http://public.artifacts.marlin.pro/projects/gateway_iris/" + public_version \
                 + "/bridge_iris-linux_amd64"
-            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "https://public.artifacts.marlin.pro/projects/gateway_iris/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "http://public.artifacts.marlin.pro/projects/gateway_iris/" + public_version \
                 + "/gateway_iris-linux_amd64"
         
         elif project_name == "gateway_cosmos":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "https://public.artifacts.marlin.pro/projects/gateway_cosmos/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "http://public.artifacts.marlin.pro/projects/gateway_cosmos/" + public_version \
                 + "/bridge_cosmos-linux_amd64"
-            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "https://public.artifacts.marlin.pro/projects/gateway_cosmos/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "http://public.artifacts.marlin.pro/projects/gateway_cosmos/" + public_version \
                 + "/gateway_cosmos-linux_amd64"
             
         elif project_name == "gateway_dot":
-            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "https://public.artifacts.marlin.pro/projects/gateway_dot/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["gateway"] = "http://public.artifacts.marlin.pro/projects/gateway_dot/" + public_version \
                 + "/gateway_dot-linux_amd64"
-            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "https://public.artifacts.marlin.pro/projects/gateway_dot/" + public_version \
+            d["bundles"]["linux-amd64.supervisor"]["data"]["bridge"] = "http://public.artifacts.marlin.pro/projects/gateway_dot/" + public_version \
                 + "/bridge_dot-linux_amd64"
 
         elif project_name == "marlinctl":
